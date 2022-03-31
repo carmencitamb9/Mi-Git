c57802f6254f312a270f6bb2dcdab205ab466f5b
Carmen <carmencita.mb9@gmail.com>
2022-03-31 00:27:41 -0300

    listas

---
+++ listas.py
@@ -0,0 +1,17 @@
+inicio =0
+fin=-1
+indice=2
+salto=1
+
+
+#index:      0    1     2      3      4
+lista_l= ["hola", "soy", "Nico",[1,2],3] 
+print ("\n*****elementos de la lista*****\n")
+print (lista_l[0])
+print (lista_l[1])
+print (lista_l[2])
+print (lista_l[3][0])
+print (lista_l[3][1])
+print (lista_l[4])
+print (lista_l[-1])
+
